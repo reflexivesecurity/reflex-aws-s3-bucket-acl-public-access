b27103265bba9cadf8abcff1814bec40f6051da3
Max Anderson <maxwell.c.anderson@gmail.com>
2020-04-21 11:03:35 -0700

    Fix linting issues

--- source/reflex_aws_s3_bucket_acl_public_access.py
+++ source/reflex_aws_s3_bucket_acl_public_access.py
@@ -10,14 +10,11 @@ from reflex_core import AWSRule
 class S3BucketAclPublicAccess(AWSRule):
     """ Detect when a bucket has ACL rules that grant public access. """
 
-    # TODO: Instantiate whatever boto3 client you'll need, if any.
-
     def __init__(self, event):
         super().__init__(event)
 
     def extract_event_data(self, event):
         """ Extract required event data """
-        # TODO: Extract any data you need from the triggering event.
         self.event_name = event["detail"]["eventName"]
         self.bucket_name = event["detail"]["requestParameters"]["bucketName"]
         self.non_compliant_acl_list = ["public-read", "public-read-write"]
@@ -30,8 +27,8 @@ class S3BucketAclPublicAccess(AWSRule):
         """
         if self.event_name == "CreateBucket":
             return self.is_create_bucket()
-        elif self.event_name == "PutBucketAcl":
-            return self.is_put_bucket_acl()
+        # if self.event_name == "PutBucketAcl":
+        return self.is_put_bucket_acl()
 
     def is_create_bucket(self):
         if "x-amz-acl" in self.event["detail"]["requestParameters"].keys():
@@ -48,23 +45,22 @@ class S3BucketAclPublicAccess(AWSRule):
                 if acl in self.non_compliant_acl_list:
                     return False
             return True
-        else:
-            if isinstance(self.event["detail"]["requestParameters"][
-                              "AccessControlPolicy"]["AccessControlList"][
-                              "Grant"], list):
-                for grant in self.event["detail"]["requestParameters"][
-                        "AccessControlPolicy"]["AccessControlList"]["Grant"]:
-                    if grant["Grantee"]["xsi:type"] == "Group":
-                        return False
-            if isinstance(self.event["detail"]["requestParameters"][
-                              "AccessControlPolicy"]["AccessControlList"][
-                              "Grant"], dict):
-                grant = self.event["detail"]["requestParameters"][
-                    "AccessControlPolicy"]["AccessControlList"]["Grant"]
+        if isinstance(self.event["detail"]["requestParameters"][
+                            "AccessControlPolicy"]["AccessControlList"][
+                            "Grant"], list):
+            for grant in self.event["detail"]["requestParameters"][
+                    "AccessControlPolicy"]["AccessControlList"]["Grant"]:
                 if grant["Grantee"]["xsi:type"] == "Group":
                     return False
-                return True
+        if isinstance(self.event["detail"]["requestParameters"][
+                            "AccessControlPolicy"]["AccessControlList"][
+                            "Grant"], dict):
+            grant = self.event["detail"]["requestParameters"][
+                "AccessControlPolicy"]["AccessControlList"]["Grant"]
+            if grant["Grantee"]["xsi:type"] == "Group":
+                return False
             return True
+        return True
 
     def get_remediation_message(self):
         """ Returns a message about the remediation action that occurred """
