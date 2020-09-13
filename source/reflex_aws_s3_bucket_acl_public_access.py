de2e0af98eb3f351143aa69f6ffd0ee1e058f8ef
rjulian <richard@rjulian.net>
2020-09-13 14:45:31 -0700

    Semi-automated multi account push

--- source/reflex_aws_s3_bucket_acl_public_access.py
+++ source/reflex_aws_s3_bucket_acl_public_access.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class S3BucketAclPublicAccess(AWSRule):
@@ -70,5 +70,9 @@ class S3BucketAclPublicAccess(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = S3BucketAclPublicAccess(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
